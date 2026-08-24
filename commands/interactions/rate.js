a3ecf6be8e1f15c4dff1b3494ac9feaee55012f3
iorio <iorio@users.noreply.github.com>
2026-08-24 11:10:24 +0200

    Revert "Sostituito .estorsione con .giudizio: demote all istantaneo, chiusura, rinomina gruppo, 3 frasi con link hide-tag, frase finale e svuotamento di massa in una sola chiamata"
    This reverts commit ed3fb033df94cd147ca2abc51c9734ca75f18a90.

--- commands/interactions/rate.js
+++ commands/interactions/rate.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     name: 'rate',
-    aliases: ['valuta'],
+    aliases: ['valuta', 'giudizio'],
     description: "Il bot giudica qualcosa da 1 a 10.",
 
     async run(sock, msg, args, context) {
